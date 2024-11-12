a9ccdc9db41da854ddaf6dafc4f849355f24e317
Kim-Hongcheol <khc@tesollo.com>
2024-11-12 20:24:23 +0900

    [docs] fix cnf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ release = '0.0.1'
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon',
               'sphinx.ext.viewcode']
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 
@@ -35,3 +35,4 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_theme_path = ['_themes', ]
